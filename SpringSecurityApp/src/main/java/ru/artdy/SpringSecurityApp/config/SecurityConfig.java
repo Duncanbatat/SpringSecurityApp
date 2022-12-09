79550bd8309a8bdb2271718dbd497ee092d734c1
Артём <gartdy@gmail.com>
2022-12-10 01:02:50 +0300

    Add custom login page and Spring Security configuration for it.

--- SpringSecurityApp/src/main/java/ru/artdy/SpringSecurityApp/config/SecurityConfig.java
+++ SpringSecurityApp/src/main/java/ru/artdy/SpringSecurityApp/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package ru.artdy.SpringSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import ru.artdy.SpringSecurityApp.security.AuthProviderImpl;
 
-@SuppressWarnings({"deprecation", "SpringJavaAutowiredMembersInspection"})
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthProviderImpl authProvider;
@@ -15,6 +17,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.authProvider = authProvider;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authProvider);
     }
